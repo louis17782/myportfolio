3beb40dd000f1551238028e75967f68878cb9a50
louis17782 <luissanteliz22@gmail.com>
2025-01-21 21:10:48 -0400

    configure functions and importing

--- src/scripts/mainMenu.js
+++ src/scripts/mainMenu.js
@@ -7,3 +7,43 @@ document.querySelector('.menuOpen').addEventListener('click', () => {
   document.querySelector('.blur-background').classList.toggle('active');
   document.querySelector('nav').classList.toggle('active');
 });
+
+
+const projects = document.querySelectorAll('.project');
+
+projects.forEach(project => {
+  project.addEventListener('mouseover', () => {
+    const content = project.querySelector('.content');
+    content.classList.add('show'); 
+  });
+
+  project.addEventListener('mouseout', () => {
+    const content = project.querySelector('.content');
+    content.classList.remove('show');
+  });
+});
+
+// carousel
+
+const carousel = document.getElementById('carouselimg');
+let scrollSpeed = 2;
+
+const image = carousel.children;
+const imageCount = image.length;
+
+for (let j = 0; j < imageCount; j++) {
+  for (let i = 0; i < imageCount; i++) {
+    let clone = image[i].cloneNode(true);
+    carousel.appendChild(clone);
+  }
+}
+
+function moveCarousel() {
+  carousel.scrollLeft += scrollSpeed;
+
+  if (carousel.scrollLeft >= image[0].clientWidth * imageCount) { 
+    carousel.scrollLeft = 0;
+  }
+}
+
+setInterval(moveCarousel, 20);
